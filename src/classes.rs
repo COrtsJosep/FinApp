8f2cce92b6e2a62102efa6c0328657af5905fb95
Josep <corts.josep@gmail.com>
2024-12-05 16:42:44 +0100

    Party validity checker works!

--- src/classes.rs
+++ src/classes.rs
@@ -1,99 +1,101 @@
-use chrono::prelude::*;
-use std::collections::HashMap;
+pub mod financial {
+	use chrono::prelude::*;
+	use std::collections::HashMap;
 
-struct Party {
-	financial_items: Vec<FinancialItem>,
-	currency: Currency,
-	id: u32
-}
-
-impl Party {
-	fn is_valid(&self) -> bool {
-		let mut aggregates: HashMap<(&Currency, String), f32> = HashMap::new();
-
-		for financial_item in self.financial_items{
-			let key: (&Currency, String) = match &financial_item {
-				FinancialItem::Transaction{ currency, .. } => {(currency, "T".to_string())},
-				FinancialItem::FundChange{ currency, .. } => {(currency, "F".to_string())}
-			};
-			let iteration_value: f32 = match &financial_item {
-				FinancialItem::Transaction { value, item_type, ..} => {
-					match item_type {
-						ItemType::Expense => { -1.0 * value },
-						ItemType::Income => { *value }
-					}
-				},
-				FinancialItem::FundChange { value, item_type, ..} => {
-					match item_type {
-						ItemType::Expense => { -1.0 * value },
-						ItemType::Income => { *value }
-					}
-				}
-			};
-
-			aggregates.entry(key).and_modify(|aggregate_value: &mut f32| *aggregate_value += iteration_value).or_insert(iteration_value);
-		}
-
-		for (key, val) in aggregates.iter() {
-			if (*val).abs() >= 0.01 {return false} // one or more cents off
-		}
-
-		true
+	pub struct Party {
+		pub financial_items: Vec<FinancialItem>,
+		pub id: u32
 	}
-}
 
-enum FinancialItem {
-	Transaction {value: f32,
-	             currency: Currency,
-	             date: NaiveDate,
-	             item_type: ItemType,
-	             category: String, // utilities, rent, salary, transport
-	             subcategory: String, // train, bus, hairdresser
-				 description: String,
-	             entity_id: u32},
-	FundChange {value: f32,
-	            currency: Currency,
-	            date: NaiveDate,
-				item_type: ItemType,
-	            account_id: u32}
-}
+	impl Party {
+		pub(crate) fn is_valid(&self) -> bool {
+			let mut aggregates: HashMap<&Currency, f32> = HashMap::new();
 
-enum ItemType {
-	Income,
-	Expense
-}
+			for financial_item in &self.financial_items{
+				let key: &Currency = match &financial_item {
+					FinancialItem::Transaction{ currency, .. } => {currency},
+					FinancialItem::FundChange{ currency, .. } => {currency}
+				};
+				let iteration_value: f32 = match &financial_item {
+					FinancialItem::Transaction { value, item_type, ..} => {
+						match item_type {
+							ItemType::Expense => { -1.0 * value },
+							ItemType::Income => { *value }
+						}
+					},
+					FinancialItem::FundChange { value, item_type, ..} => {
+						match item_type {
+							ItemType::Expense => { *value },
+							ItemType::Income => { -1.0 * value }
+						}
+					}
+				};
 
-enum Currency {
-	EUR,
-	CHF,
-	SEK
-}
+				aggregates.entry(key).and_modify(|aggregate_value: &mut f32| *aggregate_value += iteration_value).or_insert(iteration_value);
+			}
 
-enum EntityType {
-	Firm,
-	Human,
-	State,
-	NGO
-}
+			for (key, val) in aggregates.iter() {
+				if (*val).abs() >= 0.01 {return false} // one or more cents off
+			}
 
-enum AccountType {
-	Deposit,
-	Investment,
-	Cash
-}
+			true
+		}
+	}
 
-struct Entity {
-    name: String,
-    country: String,
-    id: u32,
-    entity_type: EntityType,
-    subcategory: String //supermarket, pharmacy, ... (?)
-}
+	pub enum FinancialItem {
+		Transaction {value: f32,
+					 currency: Currency,
+					 date: NaiveDate,
+					 item_type: ItemType,
+					 category: String, // utilities, rent, salary, transport
+					 subcategory: String, // train, bus, hairdresser
+					 description: String,
+					 entity_id: u32},
+		FundChange {value: f32,
+					currency: Currency,
+					date: NaiveDate,
+					item_type: ItemType,
+					account_id: u32}
+	}
 
-struct Account {
-    name: String,
-	country: String,
-    id: u32,
-    account_type: AccountType, // deposit/investing/pocket money,
-    initial_balance: f32
+	pub enum ItemType {
+		Income,
+		Expense
+	}
+
+	#[derive(Debug, Hash, PartialEq, Eq)]
+	pub enum Currency {
+		EUR,
+		CHF,
+		SEK
+	}
+
+	pub enum EntityType {
+		Firm,
+		Human,
+		State,
+		NGO
+	}
+
+	pub enum AccountType {
+		Deposit,
+		Investment,
+		Cash
+	}
+
+	struct Entity {
+		name: String,
+		country: String,
+		id: u32,
+		entity_type: EntityType,
+		subcategory: String //supermarket, pharmacy, ... (?)
+	}
+
+	struct Account {
+		name: String,
+		country: String,
+		id: u32,
+		account_type: AccountType, // deposit/investing/pocket money,
+		initial_balance: f32
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,49 @@
 mod classes;
 
+use classes::financial::*;
+use chrono::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+    let t1 = FinancialItem::Transaction {
+        value: 200.0,
+        currency: Currency::EUR,
+        date: NaiveDate::from_ymd(2024, 12, 1),
+        item_type: ItemType::Expense,
+        category: "Utilities".to_string(),
+        subcategory: "Electricity".to_string(),
+        description: "Monthly electricity bill".to_string(),
+        entity_id: 1,
+    };
+
+    let t2 = FinancialItem::Transaction {
+        value: 100.0,
+        currency: Currency::EUR,
+        date: NaiveDate::from_ymd(2024, 12, 1),
+        item_type: ItemType::Expense,
+        category: "Utilities".to_string(),
+        subcategory: "Electricity".to_string(),
+        description: "Monthly electricity bill".to_string(),
+        entity_id: 1,
+    };
+
+    let t3 = FinancialItem::FundChange {
+        value: 300.0,
+        currency: Currency::EUR,
+        date: NaiveDate::from_ymd(2024, 12, 2),
+        item_type: ItemType::Expense,
+        account_id: 42,
+    };
+
+    // Example data
+    let items = vec![t1, t2, t3];
+
+    let party: Party = Party {
+        financial_items: items,
+        id: 4
+    };
+
+    let validity = party.is_valid();
+    println!("Is valid? {validity}");
+
 }
