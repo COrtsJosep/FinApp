7d9379f1368e73283a6a303c472894d6b5eb9181
Josep <corts.josep@gmail.com>
2024-12-07 19:46:01 +0100

    Refactoring of classes using polymorphism (#1)

--- src/classes.rs
+++ src/classes.rs
@@ -3,7 +3,7 @@ pub mod financial {
 	use std::collections::HashMap;
 
 	pub struct Party {
-		pub financial_items: Vec<FinancialItem>,
+		pub transactions: Vec<Transaction>,
 		pub id: u32
 	}
 
@@ -11,29 +11,14 @@ pub mod financial {
 		pub(crate) fn is_valid(&self) -> bool {
 			let mut aggregates: HashMap<&Currency, f32> = HashMap::new();
 
-			for financial_item in &self.financial_items{
-				let key: &Currency = match &financial_item {
-					FinancialItem::Transaction{ currency, .. } => {currency},
-					FinancialItem::FundChange{ currency, .. } => {currency}
-				};
-				let iteration_value: f32 = match &financial_item {
-					FinancialItem::Transaction { value, item_type, ..} => {
-						match item_type {
-							ItemType::Expense => { -1.0 * value },
-							ItemType::Income => { *value }
-						}
-					},
-					FinancialItem::FundChange { value, item_type, ..} => {
-						match item_type {
-							ItemType::Expense => { *value },
-							ItemType::Income => { -1.0 * value }
-						}
-					}
-				};
+			for transaction in &self.transactions{
+				let value: f32 = transaction.get_value() * transaction.get_sign();
+				let currency: &Currency = transaction.get_currency();
 
-				aggregates.entry(key).and_modify(|aggregate_value: &mut f32| *aggregate_value += iteration_value).or_insert(iteration_value);
+				aggregates.entry(currency).and_modify(|aggregate: &mut f32| *aggregate += value).or_insert(value);
 			}
 
+			println!("Balance: {:?}", aggregates);
 			for (key, val) in aggregates.iter() {
 				if (*val).abs() >= 0.01 {return false} // one or more cents off
 			}
@@ -42,32 +27,82 @@ pub mod financial {
 		}
 	}
 
-	pub enum FinancialItem {
-		Transaction {value: f32,
-					 currency: Currency,
-					 date: NaiveDate,
-					 item_type: ItemType,
-					 category: String, // utilities, rent, salary, transport
-					 subcategory: String, // train, bus, hairdresser
-					 description: String,
-					 entity_id: u32},
-		FundChange {value: f32,
-					currency: Currency,
-					date: NaiveDate,
-					item_type: ItemType,
-					account_id: u32}
+	pub enum Transaction {
+		Earning {value: f32,
+			currency: Currency,
+			date: NaiveDate,
+			category: String, // salary, interest
+			subcategory: String, // regular salary, 13-month salary
+			description: String,
+			entity_id: u32},
+		Expense {value: f32,
+			currency: Currency,
+			date: NaiveDate,
+			category: String, // utilities, rent, transport
+			subcategory: String, // train, bus, hairdresser
+			description: String,
+			entity_id: u32},
+		Credit {value: f32,
+			currency: Currency,
+			date: NaiveDate,
+			account_id: u32},
+		Debit {value: f32,
+			currency: Currency,
+			date: NaiveDate,
+			account_id: u32}
 	}
 
-	pub enum ItemType {
-		Income,
-		Expense
+	impl Transaction {
+		fn get_value(&self) -> f32 {
+			match self {
+				Transaction::Earning {..} => 1.0,
+				Transaction::Expense {..} => -1.0,
+				Transaction::Credit  {..} => -1.0,
+				Transaction::Debit {..} => 1.0,
+			}
+		}
+
+		fn get_sign(&self) -> f32 {
+			match self {
+				Transaction::Earning { value, .. }
+				| Transaction::Expense { value, .. }
+				| Transaction::Credit { value, .. }
+				| Transaction::Debit { value, .. } => *value,
+			}
+		}
+
+		fn get_currency(&self) -> &Currency {
+			match self {
+				Transaction::Earning { currency, .. }
+				| Transaction::Expense { currency, .. }
+				| Transaction::Credit { currency, .. }
+				| Transaction::Debit { currency, .. } => currency,
+			}
+		}
 	}
+
 
 	#[derive(Debug, Hash, PartialEq, Eq)]
 	pub enum Currency {
 		EUR,
 		CHF,
 		SEK
+	}
+
+	struct Entity {
+		name: String,
+		country: String,
+		id: u32,
+		entity_type: EntityType,
+		subtype: String //supermarket, pharmacy, ... (?)
+	}
+
+	struct Account {
+		name: String,
+		country: String,
+		id: u32,
+		account_type: AccountType,
+		initial_balance: f32
 	}
 
 	pub enum EntityType {
@@ -81,21 +116,5 @@ pub mod financial {
 		Deposit,
 		Investment,
 		Cash
-	}
-
-	struct Entity {
-		name: String,
-		country: String,
-		id: u32,
-		entity_type: EntityType,
-		subcategory: String //supermarket, pharmacy, ... (?)
-	}
-
-	struct Account {
-		name: String,
-		country: String,
-		id: u32,
-		account_type: AccountType, // deposit/investing/pocket money,
-		initial_balance: f32
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,31 @@ use chrono::prelude::*;
 
 fn main() {
     println!("Hello, world!");
-    let t1 = FinancialItem::Transaction {
+
+    let t1 = Transaction::Expense {
         value: 200.0,
         currency: Currency::EUR,
         date: NaiveDate::from_ymd(2024, 12, 1),
-        item_type: ItemType::Expense,
         category: "Utilities".to_string(),
         subcategory: "Electricity".to_string(),
         description: "Monthly electricity bill".to_string(),
         entity_id: 1,
     };
 
-    let t2 = FinancialItem::Transaction {
+    let t2 = Transaction::Expense {
         value: 100.0,
         currency: Currency::EUR,
         date: NaiveDate::from_ymd(2024, 12, 1),
-        item_type: ItemType::Expense,
         category: "Utilities".to_string(),
-        subcategory: "Electricity".to_string(),
-        description: "Monthly electricity bill".to_string(),
+        subcategory: "Gas".to_string(),
+        description: "Monthly gas bill".to_string(),
         entity_id: 1,
     };
 
-    let t3 = FinancialItem::FundChange {
+    let t3 = Transaction::Debit {
         value: 300.0,
         currency: Currency::EUR,
         date: NaiveDate::from_ymd(2024, 12, 2),
-        item_type: ItemType::Expense,
         account_id: 42,
     };
 
@@ -39,7 +37,7 @@ fn main() {
     let items = vec![t1, t2, t3];
 
     let party: Party = Party {
-        financial_items: items,
+        transactions: items,
         id: 4
     };
 
