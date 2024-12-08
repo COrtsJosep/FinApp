17e7bc974e5670b3883e5dd117616f27d1f5ac4f
Josep <corts.josep@gmail.com>
2024-12-08 17:09:43 +0100

    Improvement of earning table (#3)

--- src/tables.rs
+++ src/tables.rs
@@ -1,18 +1,18 @@
 pub mod tables {
     use chrono::NaiveDate;
     use polars::prelude::*;
-    use crate::classes::financial::{Currency, Transaction};
+    use crate::classes::financial::{Transaction};
 
     pub struct EarningsTable {
         pub data_frame: DataFrame
     }
 
     impl EarningsTable {
-        fn new() -> EarningsTable {
+        pub fn new() -> EarningsTable {
             let mut data_frame = DataFrame::new(vec![
                 Column::from(Series::new(PlSmallStr::from("income_id"), Vec::<u32>::new())),
                 Column::from(Series::new(PlSmallStr::from("value"), Vec::<f32>::new())),
-                Column::from(Series::new(PlSmallStr::from("currency"), Vec::<Currency>::new())),
+                Column::from(Series::new(PlSmallStr::from("currency"), Vec::<String>::new())),
                 Column::from(Series::new(PlSmallStr::from("date"), Vec::<NaiveDate>::new())),
                 Column::from(Series::new(PlSmallStr::from("category"), Vec::<String>::new())),
                 Column::from(Series::new(PlSmallStr::from("subcategory"), Vec::<String>::new())),
@@ -23,7 +23,7 @@ pub mod tables {
             Self { data_frame }
         }
 
-        fn load() -> EarningsTable {
+        pub fn load() -> EarningsTable {
             let data_frame = CsvReadOptions::default()
                 .with_infer_schema_length(None)
                 .with_has_header(true)
@@ -36,19 +36,46 @@ pub mod tables {
             Self { data_frame }
         }
 
-        fn add_record(&mut self, transaction: &Transaction::Earning) {
-            let record = df![
-                "income_id" => [self.data_frame.max("income_id") + 1],
-                "value" => [transaction.value],
-                "currency" => [transaction.currency.to_string()],
-                "date" => [transaction.date],
-                "category" => [transaction.category],
-                "subcategory" => [transaction.subcategory],
-                "description" => [transaction.description],
-                "entity_id" => [transaction.entity_id]
-            ]?;
-            self.data_frame = self.data_frame.vstack(&record)?;
-            Ok(())
+        pub fn add_record(&mut self, transaction: &Transaction) -> () {
+            if let Transaction::Earning {
+                value,
+                currency,
+                date,
+                category,
+                subcategory,
+                description,
+                entity_id
+            } = transaction {
+                let income_id: u32 = {
+                    if self.data_frame.is_empty() {println!("oooo"); 0 }
+                    else {
+                        if let AnyValue::UInt32(id) = self.data_frame.column("income_id").expect("Failed to find income_id column").max_reduce().expect("Failed to generate id").value() { id + 1 }
+                        else {panic!("Failed to create an integer id")}
+                    }
+                };
+
+                let record = df!(
+                    "income_id" => [income_id],
+                    "value" => [*value],
+                    "currency" => [currency.to_string()],
+                    "date" => [*date],
+                    "category" => [category.to_string()],
+                    "subcategory" => [subcategory.to_string()],
+                    "description" => [description.to_string()],
+                    "entity_id" => [*entity_id],
+                ).expect("Failed to create earning record");
+
+                self.data_frame = self.data_frame.vstack(&record).expect("Failed to insert earning record")
+            } else {
+                panic!("Attempted to insert non-earning into the earnings table");
+            }
+
+        }
+
+
+
+        pub fn display(&self) {
+            println!("{}", self.data_frame);
         }
     }
 
