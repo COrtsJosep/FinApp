a255a3bb9e3509a369c36b1a99da5c9d4e32a19e
Josep <corts.josep@gmail.com>
2024-12-08 13:11:50 +0100

    Added documentation for basic classes (#1)

--- src/classes.rs
+++ src/classes.rs
@@ -2,16 +2,28 @@ pub mod financial {
 	use chrono::prelude::*;
 	use std::collections::HashMap;
 
+	/// A party is a balanced set of accounting transactions that happened together and that are
+	/// related to each other.
+	/// I know "party" is not the right word for that, but it's the literal translation from
+	/// Spanish ("partida financiera"), I cannot think of a better name, and adds a festive
+	/// touch to the code.
 	pub struct Party {
 		pub transactions: Vec<Transaction>,
 		pub id: u32
 	}
 
 	impl Party {
+		/// Checks whether the party is balanced. This means that for every currency involved in
+		/// the party, the amount expended/earned is associated with a decrease/increase of funds
+		/// of the same amount and opposite sign.
+		/// For instance, a bill of 50€ grocery shopping has to be balanced with, let's say, a 50€
+		/// withdrawal from a bank account. The relationship does not need to be 1:1, for
+		/// instance, a 350 SEK bill for clothing and a 230 SEK bill for presents can be balanced
+		/// with a 500 SEK withdrawal from a bank account and an 80 SEK withdrawal from pocket money.
 		pub(crate) fn is_valid(&self) -> bool {
 			let mut aggregates: HashMap<&Currency, f32> = HashMap::new();
 
-			for transaction in &self.transactions{
+			for transaction in &self.transactions {
 				let value: f32 = transaction.get_value() * transaction.get_sign();
 				let currency: &Currency = transaction.get_currency();
 
@@ -25,8 +37,15 @@ pub mod financial {
 
 			true
 		}
+
+		/// Adds a new transaction to the party.
+		pub(crate) fn add_transaction(&mut self, transaction: Transaction) -> () {
+			self.transactions.push(transaction);
+		}
 	}
 
+	/// Basic entity of the accounting system. Earnings and expenses reflect what event provoked
+	/// the movement, credit and debit record what funds were used.
 	pub enum Transaction {
 		Earning {value: f32,
 			currency: Currency,
@@ -53,6 +72,7 @@ pub mod financial {
 	}
 
 	impl Transaction {
+		/// Value getter.
 		fn get_value(&self) -> f32 {
 			match self {
 				Transaction::Earning {..} => 1.0,
@@ -62,6 +82,7 @@ pub mod financial {
 			}
 		}
 
+		/// Sign getter.
 		fn get_sign(&self) -> f32 {
 			match self {
 				Transaction::Earning { value, .. }
@@ -71,6 +92,7 @@ pub mod financial {
 			}
 		}
 
+		/// Currency getter.
 		fn get_currency(&self) -> &Currency {
 			match self {
 				Transaction::Earning { currency, .. }
@@ -89,6 +111,7 @@ pub mod financial {
 		SEK
 	}
 
+	/// Entity to which the expense is paid or, alternatively, that hands in the income.
 	struct Entity {
 		name: String,
 		country: String,
@@ -97,6 +120,7 @@ pub mod financial {
 		subtype: String //supermarket, pharmacy, ... (?)
 	}
 
+	/// Account where funds are stored.
 	struct Account {
 		name: String,
 		country: String,
