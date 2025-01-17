4121246be734793be7247aaae742e590c0ddfe52
Josep <corts.josep@gmail.com>
2025-01-17 16:08:41 +0100

    Applied exchange rate logic to the first plot (#8)

--- src/modules/database/plotter.rs
+++ src/modules/database/plotter.rs
@@ -1,11 +1,16 @@
+use std::str::FromStr;
 use chrono::NaiveDate;
 use polars::prelude::*;
 use plotters::prelude::*;
+use crate::modules::currency_exchange::CurrencyExchange;
 use crate::modules::database::DataBase;
 use crate::modules::financial::Currency;
+use std::iter::zip;
 
 impl DataBase {
-    pub(crate) fn funds_evolution(&self, currency: &Currency) -> () {
+    pub(crate) fn funds_evolution(&self, currency_to: &Currency) -> () {
+        let currency_exchange: CurrencyExchange = CurrencyExchange::init();
+        
         let initial_balances: DataFrame = self
             .account_table
             .data_frame
@@ -26,11 +31,34 @@ impl DataBase {
             .select(["value", "currency", "date"])
             .expect("Failed to select funds table");
         
+        // First step is getting all fund changes in history, and to those, adding the initial
+        // balances of all accounts.
         funds_table = funds_table.vstack(&initial_balances).expect("Could not append new data");
+
+        // Next step is creating a new column with the exchange rates for all necessary dates and 
+        // currencies
+        let mut exchange_rates: Vec<f64> = vec![];
+        let date_iter = funds_table.column("date").unwrap().date().unwrap().as_date_iter();
+        let currency_iter = funds_table.column("currency").unwrap().str().unwrap().into_iter();
+
+        for (date, currency) in zip(date_iter, currency_iter){
+            let date = date.unwrap();
+            let currency_from = Currency::from_str(currency.unwrap()).expect("Could not find currency shortname");
+            let exchange_rate = currency_exchange.exchange(&currency_from, &currency_to, date);
+
+            exchange_rates.push(exchange_rate);
+        }
         
+        // Final data manipulation step involves adding the exchange rate column to the data_frame,
+        // converting all currencies into the target currency, grouping fund changes per natural
+        // day, expanding to all days without movements, and doing the cumsum!
         let result: DataFrame = funds_table
-            .sort(["date"], Default::default()).unwrap()
+            .with_column(Series::new("exchange_rate".into(), exchange_rates))
+            .expect("Failed to add exchange_rate column")
+            .clone()
             .lazy()
+            .select([col("date"), (col("exchange_rate") * col("value")).alias("value")])
+            .sort(["date"], Default::default())
             .group_by_dynamic(
                 col("date"),
                 [],
@@ -50,7 +78,8 @@ impl DataBase {
             .collect().expect("Failed to cumsum");
 
         println!("{}", &result);
-
+        
+        // Now comes the plotting part. First extract data as vectors.
         let dates: Vec<NaiveDate> = result
             .column("date").expect("Could not find date column")
             .date().expect("Could not convert date column to date (what the hell does that mean?)")
@@ -63,7 +92,7 @@ impl DataBase {
             .f64().expect("Could not convert date column to f64 (what the hell does that mean?)")
             .into_no_null_iter().collect();
 
-        // Create a plot
+        // Then create the plot
         let root = BitMapBackend::new("figures/funds_evolution.png", (800, 600)).into_drawing_area();
         root.fill(&WHITE).expect("Failed to fill plotting root");
 
@@ -82,7 +111,7 @@ impl DataBase {
             &RED,
         )).expect("Failed to draw line");
 
-        // Save the plot
+        // Finally save the plot
         root.present().expect("Failed to present plot");
         println!("Plot saved to 'line_plot.png'");
     }
