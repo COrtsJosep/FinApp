42ce51a09e425b8707a53dfac5bcdc9aa0e7d842
Josep <corts.josep@gmail.com>
2025-04-25 20:34:06 +0200

    Method to generate last fund movements (Closes #49)

--- src/modules/database/views.rs
+++ src/modules/database/views.rs
@@ -2,6 +2,7 @@ use crate::modules::database::{capitalize_every_word, data_frame_to_csv_string, 
 use polars::prelude::*;
 
 impl DataBase {
+    /// Returns a csv in String format with the last n transactions.
     pub(crate) fn last_transactions(&self, n: usize) -> String {
         let incomes_table: DataFrame = self
             .incomes_table
@@ -63,5 +64,46 @@ impl DataBase {
             .unwrap();
 
         data_frame_to_csv_string(&mut transactions_table.head(Some(n)))
+    }
+
+    /// Returns a csv in String format with the last n fund movements.
+    pub(crate) fn last_fund_movements(&self, n: usize) -> String {
+        let funds_table: DataFrame = self
+            .funds_table
+            .data_frame
+            .clone()
+            .lazy()
+            .select([all().exclude(["fund_movement_id"])])
+            .collect()
+            .unwrap()
+            .head(Some(n));
+
+        let accounts_table: DataFrame = self
+            .account_table
+            .data_frame
+            .clone()
+            .lazy()
+            .select([col("account_id"), col("name")])
+            .rename(["name"], ["account_name"], true)
+            .collect()
+            .unwrap();
+
+        let mut last_fund_movements = funds_table
+            .inner_join(&accounts_table, ["account_id"], ["account_id"])
+            .unwrap()
+            .lazy()
+            .sort(
+                ["date", "party_id"],
+                SortMultipleOptions::default().with_order_descending_multi([true, true]),
+            )
+            .select([all().name().map(|name| {
+                Ok(PlSmallStr::from_string(capitalize_every_word(
+                    name.replace("_", " "),
+                )))
+            })])
+            .collect()
+            .unwrap();
+
+        data_frame_to_csv_string(&mut last_fund_movements)
     }
 }
